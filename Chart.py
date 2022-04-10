0f10bd539fa0308e7ed40f34bb9983cf54362b1a
Zech123as <97283406+Zech123as@users.noreply.github.com>
2022-04-11 01:44:11 +0530

    Update Chart.py

--- Chart.py
+++ Chart.py
@@ -47,7 +47,7 @@ Index_csv_1 = Main_Dict["Index_csv_1"]
 
 Expiry =  Index_csv_1.time[len(Index_csv_1)-1]
 
-st.write(f'{Expiry}, {Expiry.strftime("%A")}')
+st.write(f'{Expiry.date()}, {Expiry.strftime("%A")}')
 
 ST_Form_2 = st.sidebar.form("St_form_2")
 
@@ -73,4 +73,53 @@ ce_atm = (round(Index_csv_2.o[Entry_Date + Entry_Time]//Index_Dist)-0)*Index_Dis
 pe_atm = (round(Index_csv_2.o[Entry_Date + Entry_Time]//Index_Dist)+1)*Index_Dist
 
 st.write(ce_atm, pe_atm)
-#percent_complete = Max_profit = j = k = 0
+
+Percentage_Completed = Max_Profit = j = k = 0
+
+
+
+
+
+Progress_Strart_time = datetime.now().replace(microsecond=0)
+
+while Entry_Date + timedelta(days = k) != Exit_Date:
+	
+	Date_Divider    = Entry_Date + timedelta(days=k+1, hours=9, minutes=7)
+	Date_Divider_DF = pd.DataFrame({"Index_Time": [Date_Divider, Date_Divider], "Index_Value": [Index_Range_Min, Index_Range_Max]})
+	
+	fig.add_trace(go.Scatter(x=Date_Divider_DF["Index_Time"], y = Date_Divider_DF["Index_Value"], name = "Index Test", yaxis="y2", mode='lines', line=dict(color='#bab6b6'), line_width=0.7, showlegend = False))
+	fig.add_vline(x= Date_Divider, line_width=0.7, line_dash="solid", line_color="#bab6b6")
+	
+	k = k + 1
+
+my_bar = st.progress(0)
+
+for i in range((Sell_Dist)[0], (Sell_Dist)[1]+1, 1):
+	
+	Final_DF = pd.DataFrame()
+	
+	ce_sell_dist, pe_sell_dist = i, -1*i
+	
+	ce_sell = Main_Dict[str(ce_atm + ce_sell_dist*Index_Dist) + 'CE']
+	pe_sell = Main_Dict[str(pe_atm + pe_sell_dist*Index_Dist) + 'PE']
+	
+	st.write((str(ce_atm + ce_sell_dist*Index_Dist) + 'CE'), (str(pe_atm + pe_sell_dist*Index_Dist) + 'PE'))
+	
+	ce_sell_entry, pe_sell_entry = ce_sell.o[Entry_Date + Entry_Time], pe_sell.o[Entry_Date + Entry_Time]
+	ce_sell_exit , pe_sell_exit  = ce_sell.o[Exit_Date + Exit_Time]  , pe_sell.o[Exit_Date + Exit_Time]
+	
+	Final_DF['Change' + str(i)] = (ce_sell_entry + pe_sell_entry) - (ce_sell['o'] + pe_sell['o'])
+	Final_DF["CE_SELL"] = "CE  (" + str(round(ce_sell_entry)).rjust(5) + " |" + ce_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
+	Final_DF["PE_SELL"] = "PE  (" + str(round(pe_sell_entry)).rjust(5) + " |" + pe_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
+	Final_DF["FINAL"] = Final_DF["CE_SELL"] + "    |    " + Final_DF["PE_SELL"]
+	
+	if Final_DF['Change' + str(i)].max() > Max_Profit:
+		Max_Profit = Final_DF['Change' + str(i)].max()
+	
+	fig.add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(i)], legendgrouptitle_text = (str(int(i/5)) + "Group"), legendgroup= int(i/5), customdata = Final_DF["FINAL"], name = str(i).rjust(4), hovertemplate='Profit: (%{y:5d} )   |   %{customdata}'))#, visible='legendonly'))
+	
+	Percentage_Completed = Percentage_Completed + 1
+	
+	my_bar.progress((Percentage_Completed)/len(range((Sell_Dist)[0], (Sell_Dist)[1]+1, 1)))
+
+st.plotly_chart(fig, use_container_width = True, config={'displayModeBar': True})
