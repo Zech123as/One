e50b5fdc77bc363b322e86617cc2400619701c07
Zech123as <97283406+Zech123as@users.noreply.github.com>
2022-04-11 03:07:00 +0530

    Update Chart.py

--- Chart.py
+++ Chart.py
@@ -52,6 +52,8 @@ ST_Form_2 = st.sidebar.form("St_form_2")
 
 Entry_Date, Exit_Date = ST_Form_2.select_slider("Entry & Exit Date Inputs", options = Index_csv_1.time, value = (Index_csv_1.time[0], Index_csv_1.time[len(Index_csv_1.time)-1]), format_func = lambda x: x.date())
 Time_Input = ST_Form_2.slider("Entry & Exit Time Inputs", min_value = time(9, 15), max_value = time(15, 30), value = (time(9, 30), time(15, 30)), step = timedelta(minutes = 15))
+Buy_Lots  = ST_Form_2.slider("No of Buy Lots", min_value = 0, max_value = 15, value = 5)
+Buy_Dist  = ST_Form_2.slider("Buy Distance", min_value = 0, max_value = 30, value = 15)
 Sell_Dist = ST_Form_2.slider("Sell Distance", min_value = -15, max_value = 40, value = (-15, 20))
 
 ST_Form_2.form_submit_button("Submit")
@@ -71,11 +73,7 @@ fig = go.Figure(layout = go.Layout(yaxis=dict(domain=[0, 0.69]), yaxis2=dict(dom
 ce_atm = (round(Index_csv_2.o[Entry_Date + Entry_Time]//Index_Dist)-0)*Index_Dist
 pe_atm = (round(Index_csv_2.o[Entry_Date + Entry_Time]//Index_Dist)+1)*Index_Dist
 
-st.write(ce_atm, pe_atm)
-
 Max_Profit = j = k = 0
-
-
 
 
 
@@ -94,7 +92,7 @@ for i in range((Sell_Dist)[0], (Sell_Dist)[1]+1, 1):
 	
 	Final_DF = pd.DataFrame()
 	
-	ce_sell_dist, pe_sell_dist = i, -1*i
+	ce_sell_dist, pe_sell_dist = i, -i
 	
 	ce_sell = Main_Dict[str(ce_atm + ce_sell_dist*Index_Dist) + 'CE'].reindex(pd.date_range(Entry_Date + Entry_Time, Exit_Date + Exit_Time, freq = '1min')).between_time('09:16','15:30')
 	pe_sell = Main_Dict[str(pe_atm + pe_sell_dist*Index_Dist) + 'PE'].reindex(pd.date_range(Entry_Date + Entry_Time, Exit_Date + Exit_Time, freq = '1min')).between_time('09:16','15:30')
@@ -102,15 +100,27 @@ for i in range((Sell_Dist)[0], (Sell_Dist)[1]+1, 1):
 	ce_sell_entry, pe_sell_entry = ce_sell.o[Entry_Date + Entry_Time], pe_sell.o[Entry_Date + Entry_Time]
 	ce_sell_exit , pe_sell_exit  = ce_sell.o[Exit_Date + Exit_Time]  , pe_sell.o[Exit_Date + Exit_Time]
 	
-	Final_DF['Change' + str(i)] = (ce_sell_entry + pe_sell_entry) - (ce_sell['o'] + pe_sell['o'])
-	Final_DF["CE_SELL"] = "CE  (" + str(round(ce_sell_entry)).rjust(5) + " |" + ce_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
-	Final_DF["PE_SELL"] = "PE  (" + str(round(pe_sell_entry)).rjust(5) + " |" + pe_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
-	Final_DF["FINAL"] = Final_DF["CE_SELL"] + "    |    " + Final_DF["PE_SELL"]
+	
+	ce_buy_dist, pe_buy_dist = i + Buy_Dist, -i-Buy_Dist
+	
+	ce_buy = Main_Dict[str(ce_atm + ce_buy_dist*Index_Dist) + 'CE'].reindex(pd.date_range(Entry_Date + Entry_Time, Exit_Date + Exit_Time, freq = '1min')).between_time('09:16','15:30')
+	pe_buy = Main_Dict[str(pe_atm + pe_buy_dist*Index_Dist) + 'PE'].reindex(pd.date_range(Entry_Date + Entry_Time, Exit_Date + Exit_Time, freq = '1min')).between_time('09:16','15:30')
+	
+	ce_buy_entry, pe_buy_entry = ce_buy.o[Entry_Date + Entry_Time], pe_buy.o[Entry_Date + Entry_Time]
+	ce_buy_exit , pe_buy_exit  = ce_buy.o[Exit_Date + Exit_Time]  , pe_buy.o[Exit_Date + Exit_Time]
+	
+	
+	
+	Final_DF['Change' + str(i)] = ((ce_sell_entry + pe_sell_entry) - (ce_sell['o'] + pe_sell['o'])) + (((ce_buy['o'] + pe_buy['o']) - (ce_buy_entry + pe_buy_entry))*Buy_Lots)
+	#Final_DF["CE_SELL"] = "CE  (" + str(round(ce_sell_entry)).rjust(5) + " |" + ce_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
+	#Final_DF["PE_SELL"] = "PE  (" + str(round(pe_sell_entry)).rjust(5) + " |" + pe_sell['o'].round().astype(int).astype(str).str.rjust(5) + " )"
+	#Final_DF["FINAL"] = Final_DF["CE_SELL"] + "    |    " + Final_DF["PE_SELL"]
 	
 	if Final_DF['Change' + str(i)].max() > Max_Profit:
 		Max_Profit = Final_DF['Change' + str(i)].max()
 	
-	fig.add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(i)], legendgrouptitle_text = (str(int(i/5)) + "Group"), legendgroup= int(i/5), customdata = Final_DF["FINAL"], name = str(i).rjust(4), hovertemplate='Profit: (%{y:5d} )   |   %{customdata}'))#, visible='legendonly'))
+	fig.add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(i)], legendgrouptitle_text = (str(int(i/5)) + "Group"), legendgroup= int(i/5), name = str(i).rjust(4), hovertemplate='Profit: (%{y:5d} )'))
+	#fig.add_trace(go.Scatter(x=Final_DF.index, y=Final_DF["Change"+str(i)], legendgrouptitle_text = (str(int(i/5)) + "Group"), legendgroup= int(i/5), customdata = Final_DF["FINAL"], name = str(i).rjust(4), hovertemplate='Profit: (%{y:5d} )   |   %{customdata}'))#, visible='legendonly'))
 	
 fig.add_trace(go.Scatter(x= Index_csv_2.index, y= Index_csv_2["o"], yaxis="y2", name = Index_Name, line=dict(color='blue'), line_width=0.8, legendrank = 1))
 
